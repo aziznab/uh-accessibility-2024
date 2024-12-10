1cf2963485a2ffea9ff4dd1041b810698793e996
aziznab <aziznabiev241@outlook.com>
2024-12-10 23:48:06 +0000

    Added Java script connection and button Show More in about Me section

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('revealMore').addEventListener('click', function() {
+    var moreInfo = document.getElementById('moreInfo');
+    if (moreInfo.classList.contains('show')) {
+        moreInfo.classList.remove('show');
+        this.textContent = 'Reveal More About Me';
+    } else {
+        moreInfo.classList.add('show');
+        this.textContent = 'Show Less';
+    }
+});
